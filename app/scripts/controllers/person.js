03a14a4e157aa6cdf510fa61257030339223e83d
Fabian Vogler <fabian@equivalence.ch>
2017-09-26 11:06:36 +0200

    Don’t override accreditation info during sync

--- app/scripts/controllers/person.js
+++ app/scripts/controllers/person.js
@@ -219,7 +219,7 @@ angular.module('accreditationApp')
 	};
 
     $scope.sync = function() {
-        $scope.accreditation.$sync({eventId: $scope.eventId}, function (response) { 
+        Accreditation.sync({eventId: $scope.eventId, id: $scope.accreditation.id}, function (response) { 
             $scope.synced = true;
             $scope.eas = response.data;
         }, $rootScope.errorHandler);
