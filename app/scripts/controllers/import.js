b9b83d719003d01d109d491a25bed3f1f80cde5d
Fabian Vogler <fabian@equivalence.ch>
2017-09-28 20:00:14 +0200

    Increase import delay

--- app/scripts/controllers/import.js
+++ app/scripts/controllers/import.js
@@ -53,7 +53,7 @@ angular.module('accreditationApp').controller('ImportCtrl', function ($scope, $r
 
         $scope.importing = true;
 
-        var delay = 3000;
+        var delay = 5000;
 
         angular.forEach($scope.accreditations.data, function (accreditation, i) {
             $timeout(function () {
