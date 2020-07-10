0dbcb717cd3586e5db9e8df2ae99de0dc5f5c529
Fabian Vogler <fabian@equivalence.ch>
2020-07-10 13:38:31 +0200

    Whitespace only

--- app/scripts/controllers/print.js
+++ app/scripts/controllers/print.js
@@ -3,27 +3,27 @@
 angular.module('accreditationApp')
 .controller('PrintCtrl', function ($scope, $rootScope, $state, $stateParams, Restangular, Accreditation, REST_BASE_URL) {
 
-	$scope.loading = true;
-	$scope.apiBaseUrl = REST_BASE_URL;
+    $scope.loading = true;
+    $scope.apiBaseUrl = REST_BASE_URL;
 
-	$scope.date = new Date();
+    $scope.date = new Date();
 
-	$scope.eventId = $stateParams.eventId;
-	$scope.accreditationId = $stateParams.accreditationId;
+    $scope.eventId = $stateParams.eventId;
+    $scope.accreditationId = $stateParams.accreditationId;
 
-	// load the event info
-	Restangular.one('accreditation/events', $scope.eventId).get().then(function(result) 
-		{
-			$scope.event = result;
-		}, $rootScope.errorHandler);
+    // load the event info
+    Restangular.one('accreditation/events', $scope.eventId).get().then(function(result) 
+        {
+            $scope.event = result;
+        }, $rootScope.errorHandler);
 
-	$scope.getPerson = function()
-	{
-		$scope.loading = true;
-		$scope.accreditation = Accreditation.get({eventId: $scope.eventId, id: $scope.accreditationId}, function(accreditation) {
-			$scope.loading = false;
-		}, $rootScope.errorHandler);
-	};
+    $scope.getPerson = function()
+    {
+        $scope.loading = true;
+        $scope.accreditation = Accreditation.get({eventId: $scope.eventId, id: $scope.accreditationId}, function(accreditation) {
+            $scope.loading = false;
+        }, $rootScope.errorHandler);
+    };
 
-	$scope.getPerson();
+    $scope.getPerson();
 });
