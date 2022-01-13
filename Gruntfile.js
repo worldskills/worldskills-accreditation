7dec5311432e053ee537f5bd0e326d1732818791
Fabian Vogler <fabian@equivalence.ch>
2022-01-13 16:44:56 +0100

    Include .html on all levels in usemin

--- Gruntfile.js
+++ Gruntfile.js
@@ -219,7 +219,7 @@ module.exports = function (grunt) {
 
     // Performs rewrites based on filerev and the useminPrepare configuration
     usemin: {
-      html: ['<%= yeoman.dist %>/{,*/}*.html'],
+      html: ['<%= yeoman.dist %>/**/*.html'],
       css: ['<%= yeoman.dist %>/styles/{,*/}*.css'],
       options: {
         assetsDirs: [
