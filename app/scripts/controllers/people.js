5cc54d98db3e52d8fa2edb5bd5f31985af59bd53
Fabian Vogler <fabian@equivalence.ch>
2022-10-11 09:03:13 +0200

    Revert "Improve error handler for accreditations query"
    This reverts commit 58558155130cc6248f2e9cee83fcaa7792cf0476.

--- app/scripts/controllers/people.js
+++ app/scripts/controllers/people.js
@@ -40,23 +40,6 @@ angular.module('accreditationApp')
 		$scope.members = Member.query({member_of: member, limit: 999});
 	});
 
-    var errored = function (response) {
-    	$scope.loading = false;
-        if (response.status == 401) {
-            // Unauthorized
-            window.alert('Your session has timed out. The page will now refresh and you might need to login again.');
-            // reload page
-            window.location.reload(false)
-        } else {
-            if (response.data.user_msg) {
-                alert.error('Error: ' + ' ' + response.data.code + ': ' + response.data.user_msg);
-            } else {
-                alert.error('An error has occured: ' + JSON.stringify(response.data));
-            }
-            document.body.scrollTop = document.documentElement.scrollTop = 0;
-        }
-    };
-
 	$scope.changePage = function() 
 	{
 		$scope.loading = true;
@@ -95,7 +78,7 @@ angular.module('accreditationApp')
 		Accreditation.query(query, function(result) {
 			$scope.loading = false;
 			$scope.accreditations = result;
-		}, errored);
+		}, $rootScope.errorHandler);
 	};
 
 	$scope.filterResults = function()
