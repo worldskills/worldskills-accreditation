e65ade08f76ee9d95619e77e4991da16d1492d03
Fabian Vogler <fabian@equivalence.ch>
2023-09-15 11:47:07 +0200

    Add Member filter

--- app/scripts/controllers/scans.js
+++ app/scripts/controllers/scans.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('accreditationApp').controller('ScansCtrl', function ($scope, $rootScope, $stateParams, $filter, Scan, DelegateType, Zone) {
+angular.module('accreditationApp').controller('ScansCtrl', function ($scope, $rootScope, $stateParams, $filter, Scan, DelegateType, Zone, Member) {
 
 	$scope.loading = true;
 
@@ -9,6 +9,7 @@ angular.module('accreditationApp').controller('ScansCtrl', function ($scope, $ro
 
 	$scope.clear = function() {
 		$scope.filter.delegateType = undefined;
+		$scope.filter.member = undefined;
 		$scope.filter.zone = undefined;
 		$scope.filterResults();
 	};
@@ -16,6 +17,14 @@ angular.module('accreditationApp').controller('ScansCtrl', function ($scope, $ro
 	$scope.delegateTypes = DelegateType.query({eventId: $stateParams.eventId});
 
 	$scope.zones = Zone.query({eventId: $stateParams.eventId});
+
+	$scope.event.$promise.then(function () {
+		var member = 1;
+		if ($scope.event.member_id) {
+			member = $scope.event.member_id;
+		}
+		$scope.members = Member.query({member_of: member, limit: 999});
+	});
 
 	$scope.filterResults = function()
 	{
@@ -29,6 +38,9 @@ angular.module('accreditationApp').controller('ScansCtrl', function ($scope, $ro
 		}
 		if ($scope.filter.zone) {
 			query['zone'] = $scope.filter.zone;
+		}
+		if ($scope.filter.member) {
+			query['member'] = $scope.filter.member;
 		}
 		Scan.query(query, function(result) {
 			$scope.loading = false;
@@ -46,6 +58,8 @@ angular.module('accreditationApp').controller('ScansCtrl', function ($scope, $ro
 				'first_name',
 				'last_name',
 				'delegate_type',
+				'member_code',
+				'member_name',
 				'zone_code',
 				'zone_name',
 				'allowed'
@@ -58,6 +72,8 @@ angular.module('accreditationApp').controller('ScansCtrl', function ($scope, $ro
 				scan.accreditation.first_name,
 				scan.accreditation.last_name,
 				scan.accreditation.delegate_type.name,
+				scan.accreditation.member.code,
+				scan.accreditation.member.name.text,
 				scan.zone.code,
 				scan.zone.name,
 				scan.allowed
