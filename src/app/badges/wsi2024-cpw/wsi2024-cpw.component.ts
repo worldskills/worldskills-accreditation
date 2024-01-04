f132fa103336414a4647117d3b11686cab3b93be
Fabian Vogler <fabian@equivalence.ch>
2024-01-04 15:26:24 +0100

    Add textfit

--- src/app/badges/wsi2024-cpw/wsi2024-cpw.component.ts
+++ src/app/badges/wsi2024-cpw/wsi2024-cpw.component.ts
@@ -2,6 +2,8 @@ import {Component, Input} from '@angular/core';
 import {PersonAccreditationSummary} from "../../../types/person-accreditation-summary";
 import {Event} from "../../../types/event";
 
+declare var textFit: any;
+
 @Component({
   selector: 'badge-wsi2024-cpw',
   templateUrl: './wsi2024-cpw.component.html',
@@ -11,4 +13,10 @@ export class Wsi2024CpwComponent {
   @Input() pa: PersonAccreditationSummary;
   @Input() currentEvent: Event
   currentDate = new Date();
+
+  ngOnInit() {
+    setTimeout(() => { 
+      textFit(document.getElementsByClassName('ws-text-fit'), {multiLine: true});
+    }, 100);
+  }
 }
