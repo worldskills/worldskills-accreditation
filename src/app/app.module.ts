ae38e3cd5857344b93dd5daa4f575aad08805616
Fabian Vogler <fabian@equivalence.ch>
2024-01-23 08:50:25 +0100

    Update translation version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -48,7 +48,7 @@ import {WebcamModule} from "ngx-webcam";
 import {WebcamCaptureComponent} from './webcam-capture/webcam-capture.component';
 
 export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, '/assets/i18n/', '.json?v=20240118');
+  return new TranslateHttpLoader(http, '/assets/i18n/', '.json?v=20240123');
 }
 
 @NgModule({
