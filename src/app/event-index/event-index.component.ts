06f0506b09759fe082cb847dfd76b13a01532771
hengkysanjaya123 <hengkysanjaya204@gmail.com>
2024-08-21 22:39:57 +0700

    Tweak EventIndex permission checks

--- src/app/event-index/event-index.component.ts
+++ src/app/event-index/event-index.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {NgAuthService, UserRoleUtil, WsComponent} from "@worldskills/worldskills-angular-lib";
-import {EventService} from "../../services/event/event.service";
 import {ActivatedRoute, Router} from "@angular/router";
 import {environment} from "../../environments/environment";
 
@@ -11,10 +10,10 @@ import {environment} from "../../environments/environment";
 export class EventIndexComponent extends WsComponent implements OnInit {
 
   constructor(
-              private router: Router,
-              private route: ActivatedRoute,
-              private authService: NgAuthService,
-            ) {
+    private router: Router,
+    private route: ActivatedRoute,
+    private authService: NgAuthService,
+  ) {
     super();
   }
 
@@ -27,11 +26,17 @@ export class EventIndexComponent extends WsComponent implements OnInit {
         const hasPrintPermission = UserRoleUtil.userHasRoles(currentUser, environment.worldskillsAppId, environment.appRoles.ADMIN, environment.appRoles.PRINT);
         const hasEditVehiclePermission = UserRoleUtil.userHasRoles(currentUser, environment.worldskillsAppId, environment.appRoles.ADMIN, environment.appRoles.EDIT_VEHICLES);
         const hasPrintVehiclePermission = UserRoleUtil.userHasRoles(currentUser, environment.worldskillsAppId, environment.appRoles.ADMIN, environment.appRoles.PRINT_VEHICLES);
+        const hasAllocateZoneRequestPermission = UserRoleUtil.userHasRoles(currentUser, environment.worldskillsAppId, environment.appRoles.ADMIN, environment.appRoles.ALLOCATE_ZONE_REQUEST);
 
         if (hasEditPermission || hasPrintPermission) {
           this.router.navigate(['people'], {relativeTo: this.route});
-        } else {
+        } else if (hasEditVehiclePermission || hasPrintVehiclePermission) {
           this.router.navigate(['vehicles'], {relativeTo: this.route});
+        } else if(hasAllocateZoneRequestPermission){
+          this.router.navigate(['zone-request-form'], {relativeTo: this.route});
+        } else {
+          // TODO: to consider having it in event.component.ts? - #SleepyForNow
+          // this.router.navigate([''], {relativeTo: this.route});
         }
       })
     )
