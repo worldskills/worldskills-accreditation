c93a0aa8dc4f3a6026f944f6c6cba199f08d80a1
Fabian Vogler <fabian@equivalence.ch>
2024-09-05 17:45:56 +0200

    Invalidate translations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -78,7 +78,7 @@ import { EventIndexComponent } from './event-index/event-index.component';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 
 export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, '/assets/i18n/', '.json?v=20240830110126');
+  return new TranslateHttpLoader(http, '/assets/i18n/', '.json?v=20240905174532');
 }
 
 @NgModule({
