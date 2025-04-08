558aa0f8f20e316ade1a8e27dd56a671a4d9665c
hengkysanjaya123 <hengkysanjaya204@gmail.com>
2025-04-08 10:08:45 +0800

    Use textFit for CIW2025 badge

--- src/app/badges/wsi2025-ciw/wsi2025-ciw.component.ts
+++ src/app/badges/wsi2025-ciw/wsi2025-ciw.component.ts
@@ -1,6 +1,8 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, SimpleChanges} from '@angular/core';
 import {PersonAccreditationSummary} from "../../../types/person-accreditation-summary";
 import {Event} from "../../../types/event";
+
+declare var textFit: any;
 
 @Component({
   selector: 'badge-wsi2025-ciw',
@@ -12,4 +14,26 @@ export class Wsi2025CiwComponent {
   @Input() pa: PersonAccreditationSummary;
   @Input() currentEvent: Event
   currentDate = new Date();
+  showComponent = true;
+
+  ngOnInit() {
+    this.refreshComponent();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['pa']) {
+      this.refreshComponent();
+    }
+  }
+
+  refreshComponent() {
+    this.showComponent = false;
+    setTimeout(() => {
+      this.showComponent = true;
+      setTimeout(() => {
+        textFit(document.getElementsByClassName('ws-text-fit'), {alignVert: true});
+      }, 100);
+    }, 100);
+  }
+
 }
